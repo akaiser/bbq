c25e3126511233a4669a612dd2b8f8546e5d9469
Albert Kaiser <ak@kaiserv.de>
2021-07-21 20:26:42 +0200

    test release script

---
+++ _release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd bbq_app/
+flutter clean
+flutter build apk --split-per-abi
+
+cd ..
+rm -r bbq_be/release/*.apk
+cp bbq_app/build/app/outputs/apk/release/app-* bbq_be/release/
+
+cd bbq_be/
+git add .
+git commit -m "update release"
+git push
+
+cd ..
+git add .
+git commit -m "update subs"
+git push
